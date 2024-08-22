188e7b0a711ae8ae9adf553b766e2f73f1b66a0e
intelercorer <egor.vihlyantsev@yandex.ru>
2024-08-22 14:48:47 +0300

    Commit

--- web-app/main.py
+++ web-app/main.py
@@ -1,14 +1,25 @@
 import os
 import pandas as pd
-from flask import Flask, render_template, request, redirect, url_for, send_file
+from flask import Flask, render_template, request, redirect, url_for, send_file, flash
 import joblib
 import matplotlib.pyplot as plt
+import matplotlib
 import numpy as np
 
 app = Flask(__name__)
 
-model = joblib.load('../predictor/SVMScaled.joblib')
+svm_model = joblib.load('../predictor/SVMScaled.joblib')
+rf_model = joblib.load('../predictor/RFScaled.joblib')
+xgb_model = joblib.load('../predictor/XGBScaled.joblib')
+lr_model = joblib.load('../predictor/LRScaled.joblib')
 scaler = joblib.load('../predictor/scaler.joblib')
+
+model_dict = {
+    'SVM': svm_model,
+    'RandomForest' : rf_model,
+    'Xgboost': xgb_model,
+    'Logreg': lr_model
+}
 
 prediction_df = None
 
@@ -24,7 +35,7 @@ def generate_recommendations(predictions):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', models_data=model_dict.keys())
 
 
 @app.route('/upload', methods=['POST'])
@@ -53,8 +64,15 @@ def upload_file():
             features = df.drop(columns=['RowNumber', 'CustomerId', 'Tenure', 'EstimatedSalary', 'Surname', 'NumOfProducts', 'HasCrCard', 'Exited'])  # Modify as per your dataset
             features = scaler.transform(features)
 
+            selected_model = request.form.get('models')
+            #print(selected_model)
+            model = model_dict[selected_model]
+
             predictions = model.predict(features)
             probabilities = model.predict_proba(features)[:, 1]
+
+            count_0 = (predictions == 0).sum()
+            count_1 = (predictions == 1).sum()
 
             prediction_df = pd.DataFrame({
                 'CustomerId': df['CustomerId'],
@@ -66,13 +84,15 @@ def upload_file():
 
             prediction_table = prediction_df.to_dict(orient='records')
 
+            matplotlib.use('Agg')
             plt.figure()
-            plt.pie(np.unique(predictions, return_counts= True)[1], labels=['Нет риска', 'Группа риска'], shadow=True, autopct='%1.1f%%', startangle=180)
+            plt.pie(np.unique(predictions, return_counts=True)[1], labels=['Нет риска', 'Группа риска'], shadow=True, autopct='%1.1f%%', startangle=180)
             plt.title('Диаграмма предсказаний')
             plot_path = os.path.join('static', 'plot.png')
             plt.savefig(plot_path)
+            plt.close()
 
-            return render_template('index.html', predictions=prediction_table, visualization=True)
+            return render_template('index.html', predictions=prediction_table, visualization=True, models_data=model_dict.keys(), selected_model=selected_model, count_0=count_0, count_1=count_1, dataset_size=len(df))
 
     return redirect(url_for('index'))
 
